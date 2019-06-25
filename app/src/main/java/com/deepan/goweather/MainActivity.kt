5b15fbf768872eec584df1ebe1034155cb5d1526
Deepan <deepan.e@zohocorp.com>
2019-06-26 02:42:11 +0530

    Progress bar added.

--- app/src/main/java/com/deepan/goweather/MainActivity.kt
+++ app/src/main/java/com/deepan/goweather/MainActivity.kt
@@ -3,13 +3,15 @@ package com.deepan.goweather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.animation.DecelerateInterpolator
+import android.animation.ObjectAnimator
+import android.view.animation.AnimationUtils
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        progress.indeterminateDrawable = resources.getDrawable(R.drawable.ic_loading)
     }
 }
